df5ad6cb0f85ccd4f02f61b73c52508204d8f4e0
mikegn00 <michaelgn00@gmail.com>
2022-11-24 20:23:16 +0000

    Add project files.

---
+++ calculator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool Pressed { get; set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Pressed = false;
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Pressed = !Pressed;
+            lable.Content = Pressed ? "ON" : "OFF";
+        }
+    }
+}
